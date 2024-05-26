e5523a979d5c1a9313ad34e93618ae89e1dbe5bf
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2024-05-26 20:33:20 +0545

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,7 +5,7 @@ import string
 
 def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
 
-    # Define the possible characters for the password
+    # Define the possible characters for the  password
     letters = string.ascii_letters
     digits = string.digits
     symbols = string.punctuation
